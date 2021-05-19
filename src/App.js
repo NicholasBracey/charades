9a7c973c7240902dd8a4861a7b786b9ecf92d698
NicholasBracey <nicholasbracey@gmail.com>
2021-05-19 17:32:54 +0100

    http changed to https

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import { BsDisplay, BsBook } from "react-icons/bs";
 
 import Card from './components/card/Card';
 
-
+// api GET calls + .env api keys
 const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_KEY;
-const MOVIE_API_URL = `http://api.themoviedb.org/3/movie/top_rated?api_key=${MOVIE_API_KEY}`;
-const TV_API_URL = `http://api.themoviedb.org/3/tv/popular?api_key=${MOVIE_API_KEY}`;
+const MOVIE_API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${MOVIE_API_KEY}`;
+const TV_API_URL = `https://api.themoviedb.org/3/tv/popular?api_key=${MOVIE_API_KEY}`;
 
 const BOOK_API_KEY = process.env.REACT_APP_BOOK_KEY;
 const BOOK_API_URL = `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=${BOOK_API_KEY}`;
 
 const MUSIC_API_KEY = process.env.REACT_APP_MUSIC_KEY;
-const MUSIC_API_URL = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${MUSIC_API_KEY}&format=json`;
+const MUSIC_API_URL = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${MUSIC_API_KEY}&format=json`;
 
 
 const App = () => {
+  //setState
   const [movieTitle, setMovieTitle] = useState('');
   const [tvTitle, setTvTitle] = useState('');
   const [bookTitle, setBookTitle] = useState('');
@@ -29,11 +30,13 @@ const App = () => {
   const randomNumber = Math.floor(Math.random() * 15) + 0;
 
   const fetchData = () => {
+    // axios api calls
     const getMovieApi = axios.get(MOVIE_API_URL);
     const getTvApi = axios.get(TV_API_URL);
     const getBookApi = axios.get(BOOK_API_URL);
     const getMusicApi = axios.get(MUSIC_API_URL);
 
+    // spread data + drill into object for relevent info
     axios.all([getMovieApi, getTvApi, getBookApi, getMusicApi]).then(
       axios.spread((...allData) => {
         const allMovieData = allData[0].data.results[randomNumber].title;
