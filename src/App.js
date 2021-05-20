4c237b67d4a303905952c03e523031e4ff06e509
NicholasBracey <nicholasbracey@gmail.com>
2021-05-20 18:00:17 +0100

    styles added

--- src/App.js
+++ src/App.js
@@ -20,28 +20,50 @@ const App = () => {
   const [bookTitle, setBookTitle] = useState("");
   const [musicInfo, setMusicInfo] = useState({ track: "", artist: "" });
 
-  // get a random book/movie from a object of 15 results
-  const randomNumber = Math.floor(Math.random() * 15) + 0;
-
   const fetchData = () => {
     // axios api calls
     const getMovieApi = axios.get(MOVIE_API_URL);
     const getTvApi = axios.get(TV_API_URL);
     const getBookApi = axios.get(BOOK_API_URL);
     const getMusicApi = axios.get(MUSIC_API_URL);
+    // bundle api's together
+    const promises = [getMovieApi, getTvApi, getBookApi, getMusicApi];
+    // catch error
+    const promisesResolved = promises.map((promise) =>
+      promise.catch((error) => ({ error }))
+    );
+    //if error show response
+    function checkFailed(then) {
+      return function (responses) {
+        const someFailed = responses.some((response) => response.error);
 
-    // spread data + drill into object for relevent info
+        if (someFailed) {
+          throw responses;
+        }
+
+        return then(responses);
+      };
+    }
+    // check each api call for errors
+    axios.all(promisesResolved)
+      .then(
+        checkFailed(([getMovieApi, getTvApi, getBookApi, getMusicApi]) => {
+          console.log(
+            "SUCCESS",
+            getMovieApi,
+            getTvApi,
+            getBookApi,
+            getMusicApi
+          );
+        })
+      )
+      // if error, log FAIL
+      .catch((err) => {
+        console.log("FAIL", err);
+      });
+      // drill into api and setState
     axios.all([getMovieApi, getTvApi, getBookApi, getMusicApi])
-    .catch(err => {
-      if (err.response) {
-        // client received an error response (5xx, 4xx)
-      } else if (err.request) {
-        // client never received a response, or request never left
-      } else {
-        // anything else
-      }
-  })
-    .then(
+      .then(
       axios.spread((...allData) => {
         const allMovieData = allData[0].data.results[randomNumber].title;
         const allTvData = allData[1].data.results[randomNumber].name;
@@ -57,21 +79,22 @@ const App = () => {
         setMusicInfo(allMusicData);
       })
     );
-    
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  // get a random book/movie from a object of 15 results
+  const randomNumber = Math.floor(Math.random() * 15) + 0;
 
   // convert book result in Title Case
-  const toTitleCase = (phrase) => {
-    return phrase
+  const toTitleCase = (bookTitle) => {
+    return bookTitle
       .toLowerCase()
-      .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
   };
 
   let book = toTitleCase(bookTitle);
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,12 +5,42 @@ import { ImVideoCamera } from "react-icons/im";
 import { BsDisplay, BsBook } from "react-icons/bs";
 
 export const Card = ({ track, artist, movie, tv, book }) => {
+  // button for new card (refresh page)
+  const newCard = () => {
+    window.location.reload();
+  };
   return (
-    <div className={styles.card}>
-      <div><FaMusic /> : {track} - {artist}</div>
-      <div><ImVideoCamera /> : {movie}</div>
-      <div><BsDisplay /> : {tv}</div>
-      <div><BsBook /> : {book}</div>
+    <div className={styles.card_wrapper}>
+      <div className={styles.card}>
+        <div className={styles.card_item}>
+          <div className={styles.card_icon}>
+            <FaMusic size={50} />
+          </div>
+          <p>
+            {track} <br />- {artist}
+          </p>
+        </div>
+        <div className={styles.card_item}>
+          <div className={styles.card_icon}>
+            <ImVideoCamera size={50} />
+          </div>
+          <p>{movie}</p>
+        </div>
+        <div className={styles.card_item}>
+          <div className={styles.card_icon}>
+            <BsDisplay size={50} />
+          </div>
+          <p>{tv}</p>
+        </div>
+        <div className={styles.card_item}>
+          <div className={styles.card_icon}>
+            <BsBook size={50} />
+          </div>
+          <p>{book}</p>
+        </div>
+        <button onClick={newCard} className={`${styles.card_btn} ${styles.btn_color}`}>New card</button>
+      </div>
+      
     </div>
   );
 };
